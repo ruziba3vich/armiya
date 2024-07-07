00ae8c822053877c8fa1df19e07e5dcc7b09ff52
nodirbek <nomonovn2@gmail.com>
2024-07-07 05:13:23 -0700

    scripts optimized

---
+++ equipment-service/migrations/20240614132632_centerx_db.up.sql
@@ -0,0 +1,6 @@
+INSERT INTO ammo_types (name, caliber, type, description) VALUES
+                                                              ('5.56mm NATO', '5.56mm', 'Rifle', 'Standard NATO rifle ammunition'),
+                                                              ('7.62mm NATO', '7.62mm', 'Rifle', 'Standard NATO rifle ammunition for larger caliber rifles'),
+                                                              ('9mm Parabellum', '9mm', 'Pistol', 'Common pistol ammunition used by many military forces'),
+                                                              ('12 Gauge', '12 Gauge', 'Shotgun', 'Standard shotgun ammunition'),
+                                                              ('40mm Grenade', '40mm', 'Grenade', 'Used in grenade launchers for explosive impact');
